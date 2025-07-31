d2e760383fa974c082608e1877eca0dacb63f742
luciahelenasalvi <luciarissi.salvi@gmail.com>
2025-07-30 22:42:08 -0300

    Inserção Responsive Voice

--- app.js
+++ app.js
@@ -10,12 +10,30 @@ let amigoSorteado;
 function exibirTextoNaTela(tag, texto){
     let campo = document.querySelector(tag);
     campo.innerHTML = texto;
+
+     if ('speechSynthesis' in window) {
+        let utterance = new SpeechSynthesisUtterance(texto);
+        utterance.lang = 'pt-BR'; 
+        utterance.rate = 1.2; 
+        window.speechSynthesis.speak(utterance); 
+    } else {
+        console.log("Web Speech API não suportada neste navegador.");
+    }
 }
 
 // exibirResultado
 function exibirResultado(texto) {
     const campoResultado = document.querySelector(".result-list");
     campoResultado.textContent = texto; // ou innerHTML se quiser HTML
+
+     if ('speechSynthesis' in window) {
+        let utterance = new SpeechSynthesisUtterance(texto);
+        utterance.lang = 'pt-BR'; 
+        utterance.rate = 1.2; 
+        window.speechSynthesis.speak(utterance); 
+    } else {
+        console.log("Web Speech API não suportada neste navegador.");
+    }
 }
 
 
