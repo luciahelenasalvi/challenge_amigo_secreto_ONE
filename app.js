88d968b3bd0f401a30fb335f8fbbeb65534e1300
luciahelenasalvi <luciarissi.salvi@gmail.com>
2025-07-30 22:23:13 -0300

    Testes de funcionamento e acertos

--- app.js
+++ app.js
@@ -1,18 +1,32 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
 
+
+// Declaração de variáveis e listas
 let amigos = []; 
 let nome; 
+let amigoSorteado; 
 
-
+// Funções:
 function exibirTextoNaTela(tag, texto){
     let campo = document.querySelector(tag);
     campo.innerHTML = texto;
 }
 
+// exibirResultado
+function exibirResultado(texto) {
+    const campoResultado = document.querySelector(".result-list");
+    campoResultado.textContent = texto; // ou innerHTML se quiser HTML
+}
+
+
 function limparCampo() {
     texto = document.querySelector('input');
     texto.value = '';
 }
+
+// adicionarAmigo : a função recebe um nome pelo imput (campo de texto - index.html). 
+// Validação : Caso o campo esteja vazio, exibirá uma mensagem. Do contrário, incluirá 
+// o nome na lista
 
 function adicionarAmigo(){
  
@@ -30,7 +44,7 @@ function adicionarAmigo(){
 listarAmigos()
 }
 
-
+// listarAmigos : imprime a lista atualizada a cada vez que for inserido um nome na lista. 
 function listarAmigos(){
 
     let lista = document.getElementById('listaAmigos');
@@ -48,13 +62,14 @@ function listarAmigos(){
   
 }
 
-let amigoSorteado; 
+// sortearAmigo : sorteia um item da lista via função math.random()
 
 function sortearAmigo(lista){
   let indiceItemSorteado = Math.floor(Math.random() * amigos.length);
   amigoSorteado = amigos[indiceItemSorteado];
   //console.log(amigoSorteado); 
-  exibirTextoNaTela('h4', `O amigo sorteado é: ${amigoSorteado}`); 
+  exibirResultado(`O amigo sorteado é: ${amigoSorteado}`); 
+
   //limpar a ul. 
   exibirTextoNaTela('ul', ''); 
 
